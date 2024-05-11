cd3161301ea3092f4a3fec5da4b831c05e420778
Ahmad Mahmood <147245774+skytechofficial@users.noreply.github.com>
2024-05-11 18:28:25 +0500

    page1 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let tl = gsap.timeline()
+function loader() {
+    let tl = gsap.timeline()
 tl.from("#loader h1",{
     y:150,
     stagger:0.25,
@@ -28,3 +29,29 @@ tl.from("#page-1",{
 tl.to("#loader",{
     display:"none"
 })
+tl.from("nav",{
+    opacity:0
+})
+tl.from(".hero1 h1,.hero2 h1,.hero3 h2,.hero3 h3,.hero4 h1",{
+    y:120,
+    stagger:0.2,
+    duration:1.1
+})
+}
+function crsrAnime() {
+    document.addEventListener("mousemove",function(dets){
+        gsap.to("#crsr",{
+            left:dets.x,
+            top:dets.y,
+        })
+    })
+}
+function magnetEffect(params) {
+Shery.makeMagnet("#part-2 h4" /* Element to target.*/, {
+    //Parameters are optional.
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,});
+}
+loader()
+crsrAnime()
+magnetEffect()
